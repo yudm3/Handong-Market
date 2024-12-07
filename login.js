5f49e576b05ca06d39b9388c3e9ab0ef79ac17fe
Dmitriy Yugay <dimon.zbs.da@gmail.com>
2024-12-07 16:28:17 +0900

    renaming CSS classes and implementing responsiveness

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+// login.js
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
